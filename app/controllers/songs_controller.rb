64501a772e710e7efe82906fb02b0a3348bc637e
Emily Welsch <emwelsch@gmail.com>
2018-08-14 19:18:10 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,10 +12,10 @@ class SongsController < ApplicationController
     erb :'songs/new'
   end
 
-  # get '/songs/:slug' do
-  #   @song = Song.find_by_slug(params[:slug])
-  #   erb :'songs/show'
-  # end
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
+  end
 
   post '/songs' do
     @song = Song.create(:name => params["Name"])
